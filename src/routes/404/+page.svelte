92d7a00aca025fb21e297552230fc768c7a5558e
Thierry Kühni <thierry.kuehni@hotmail.com>
2024-01-19 15:17:04 +0100

    Add custom 404 page for GitHub pages

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    /**
+     * Custom 404 error page for GitHub pages
+     */
+
+    import ErrorMessage from "$lib/ErrorMessage.svelte";
+</script>
+
+<ErrorMessage message="Oh noes this page could not be found :[" status={404} />
